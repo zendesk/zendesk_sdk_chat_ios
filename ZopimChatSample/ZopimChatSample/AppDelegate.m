26e363d9479cab932b36a5d21f208380ff59e1d0
Dan Sessions <dansessions@gmail.com>
2019-07-29 12:09:12 +0100

    Release 1.4.1

--- ZopimChatSample/ZopimChatSample/AppDelegate.m
+++ ZopimChatSample/ZopimChatSample/AppDelegate.m
@@ -45,7 +45,9 @@
 
     // remember to switch off debug logging before app store submission!
     [ZDCLog enable:YES];
-    [ZDCLog setLogLevel:ZDCLogLevelInfo];
+    [ZDCLog setLogLevel:ZDCLogLevelWarn];
+    [[[ZDCChat instance] overlay] setEnabled:NO];
+    
 
     ////////////////////////////////////////////////////////////////////////////////////////////////
     // sample app boiler plate
@@ -53,6 +55,19 @@
 
     NSSetUncaughtExceptionHandler(&uncaughtExceptionHandler);
 
+    [self setupRootViewController];
+    
+    // push notifications
+    [self requestPermissions];
+
+    // make key window
+    [self.window makeKeyAndVisible];
+
+    return YES;
+}
+
+- (void) setupRootViewController
+{
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     self.window.backgroundColor = [UIColor colorWithWhite:0.94f alpha:1.0f];
 
@@ -65,17 +80,7 @@
     // assign nav controller as root
     self.window.rootViewController = navController;
 
-    // push notifications
-    [self requestPermissions];
-
-    // make key window
-    [self.window makeKeyAndVisible];
-
-    // notification
-    [ZDCChat didReceiveRemoteNotification:launchOptions];
-    return YES;
 }
-
 
 - (void) styleApp
 {
@@ -137,7 +142,17 @@
 
 - (void) application:(UIApplication*)application didReceiveRemoteNotification:(NSDictionary*)userInfo
 {
-    [ZDCChat didReceiveRemoteNotification:userInfo];
+    [self.window makeKeyAndVisible];
+    [self setupRootViewController];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [ZDCChat didReceiveRemoteNotification:userInfo];
+    });
+}
+
+- (BOOL) application:(UIApplication *)application shouldRestoreApplicationState:(NSCoder *)coder
+{
+    return true;
 }
 
 
